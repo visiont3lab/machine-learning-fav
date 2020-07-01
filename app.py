ee2872437db8481946430d838a3a954c7e3838d8
visiont3lab <visiont3lab@gmail.com>
2020-07-01 17:34:13 +0000

    ho aggiunto main.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def maxOfArray(arr):
+  return np.max(arr)
+
+
+a = np.array([[2,4,5],[4,5,6]])
+b = ["ñlk","kldks", 5]
+c = "casa"
+d = { "name" : 5}
+l = (3,5)
+
+arr = [a,b,c,d,l]
+
+listVal = []
+for i in range(0,4):
+  listVal.append(arr[i])
+
+print(len(listVal))
+print(a)
+print(a.shape)
+
